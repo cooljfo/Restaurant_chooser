62610138560d661702c366c09789434d00b05771
’Jean-Francois <j.f_chenier@hotmail.com>
2016-10-05 18:51:45 -0400

    first commit

---
+++ restaurant_chooser.py
@@ -0,0 +1,20 @@
+from random import randint
+
+restoList = []
+
+fo = open('restaurant.txt', 'r')
+
+for line in fo:
+	name, probability = line.split(': ')
+	for x in range(0, int(probability)):
+		restoList.append(name)
+		
+restaurant_number = randint(0,len(restoList))
+print restoList[restaurant_number]
+
+
+fo.close()
+
+
+
+
